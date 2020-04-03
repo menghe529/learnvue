5f7ece722a40e025c6520a595c677f1a0600f635
jenson <menghe529@gmail.com>
2020-04-04 07:24:40 +0800

    init

---
+++ src/components/First.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        Hello world！
+    <router-link to="/a">跳转到a</router-link>
+    <router-link to="/b">跳转到b</router-link>
+    <router-view></router-view>
+    </div>
+</template>
